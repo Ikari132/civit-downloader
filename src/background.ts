6068461e9b19d2ca9bb2fd42974e2bea2417aee1
Anton Aksionau <ikari808@gmail.com>
2023-09-19 13:16:43 +0200

    move current tab function to helpers

--- src/background.ts
+++ src/background.ts
@@ -69,12 +69,6 @@ async function handleDownload(data: IDownloadActionData) {
     updateHistory(data.modelVersion, state);
   })
 }
-async function getCurrentTab() {
-  const queryOptions = { active: true, lastFocusedWindow: true };
-  // `tab` will either be a `tabs.Tab` instance or `undefined`.
-  const [tab] = await chrome.tabs.query(queryOptions);
-  return tab;
-}
 
 chrome.runtime.onMessage.addListener((action: TAction, sender, sendResponse) => {
   const tabId = sender?.tab?.id;
--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -265,3 +265,10 @@ export async function checkHistory({ data }: ICheckHistoryAction, sendResponse: 
     sendResponse(false);
   }
 }
+
+export async function getCurrentTab() {
+  const queryOptions = { active: true, lastFocusedWindow: true };
+  // `tab` will either be a `tabs.Tab` instance or `undefined`.
+  const [tab] = await chrome.tabs.query(queryOptions);
+  return tab;
+}
